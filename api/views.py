398cb6ca5264487a330a11355691b1e31653edb2
Thierno Diallo <contact.thierno@gmail.com>
2021-12-25 17:23:58 +0000

    Add TaskDetail View & Map URL + Register Task Model to Django Admin Interface

--- api/views.py
+++ api/views.py
@@ -27,3 +27,9 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
